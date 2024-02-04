9205e1d5afa358e0ed65025a09ef51fcca746559
Mahamarakkalage Dileepa Yasas Fernando <dileepa_fernando@ECCLT2203811s-MacBook-Pro.local>
2024-02-04 11:59:44 +0800

    MoreMemAttacks added

---
+++ MoreMemAttacks/integerof.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+int imin=-2147483648;
+int imax= 2147483647;
+ // int count=0;
+
+/*printf("imax+1=%d positive addition overflow\n",imax+1);
+printf("imin-1=%d negative addition overflow\n",imin-1);
+printf("imax*2=%d multiplication overflow\n",imax*2);
+printf("7/3=%d -7/3=%d\n",7/3,-7/3);
+printf("imin/-1=%d division overflow\n",imin/-1);*/
+
+printf("100% dude\n");
+printf("100% sir\n");
+printf("%x %x %x\n")
+;
+printf("%s%s%s\n");
+  
+/*  int count=0;
+  int x=500;
+  int y=100000;
+printf("%x%x%n",x,y,&count);
+printf("check");*/
+
+
+//printf("Hello %p %n");
+
+}
